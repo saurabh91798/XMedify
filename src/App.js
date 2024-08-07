2da82d78fbc29bebc2ddfca42a1b930df17a09ab
K.Saurabh Rao <saurabh91798@gmail.com>
2024-08-07 20:51:59 +0530

    added Home and implemented the UI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-
-
-
+import { Outlet } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+import Footer from "./components/Footer/Footer";
 import DownloadApp from "./components/Sections/DownloadApp/DownloadApp";
 
 function App() {
   return (
     <div>
+      <CssBaseline />
+      <Outlet />
       <DownloadApp />
+      <Footer />
     </div>
   );
 }
